781368b2458893242d4f9e79cae845e1688bb105
julia <sjmjulia@163.com>
2017-05-22 13:57:51 +0800

    Initial Commit

---
+++ BookstoreSimpleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookstoreSimpleDemo
+//
+//  Created by ShenJiamei on 5/22/17.
+//  Copyright © 2017 REINS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
